2e882b951df85cbbf27ed2211547940fa333bddf
Natasha <khanina.nata@mail.ru>
2022-10-19 13:11:30 +0300

    Задача 13

---
+++ Tesk13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введи число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if(numberText.Length > 2)
+{
+    Console.WriteLine(numberText[2]);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
